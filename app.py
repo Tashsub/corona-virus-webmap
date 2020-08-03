8fc3d7e50a93d178a83e953d22a9e807efc30841
Tashinga Musanhu <44523844+Tashsub@users.noreply.github.com>
2020-08-03 22:13:52 +0200

    first marker

--- app.py
+++ app.py
@@ -4,16 +4,18 @@ import folium
 #takes in long and lat
 
 coord = [48.310343, 4.137183] #set to france
-locations = [[46.233232, 6.139241]]
+
+locations = {"france": [46.233232, 6.139241] }
+
 map = folium.Map(location= coord, zoom_start=5, tiles = "Stamen Terrain" )
 
 #all elements added to map will need to be added between the creation of the map 
 #and the saving of the map 
 
+map.add_child(folium.Marker(location = locations["france"], popup ="String", icon = folium.Icon(color = 'green')))
 
 
 
 
-map.Marker(locations[0])
-map.save("Map1.html")
+map.save("Map.html")
 
