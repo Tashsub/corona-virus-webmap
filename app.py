0be7153bcf5d5a899b9a14b65d5a97b1d18e0d4b
Tashinga Musanhu <44523844+Tashsub@users.noreply.github.com>
2020-08-03 22:05:03 +0200

    map showing

--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 import folium
 
-map = folium.Map
+#this is where the map will open
+#takes in long and lat
+
+coord = [48.310343, 4.137183] #set to france
+locations = [[46.233232, 6.139241]]
+map = folium.Map(location= coord, zoom_start=5, tiles = "Stamen Terrain" )
+
+#all elements added to map will need to be added between the creation of the map 
+#and the saving of the map 
+
+
+
+
+
+map.Marker(locations[0])
+map.save("Map1.html")
+
