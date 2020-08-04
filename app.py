387f9fbaed56bedcd923948dd0daf7240e96c503
Tashinga Musanhu <44523844+Tashsub@users.noreply.github.com>
2020-08-04 21:54:15 +0200

    source added. Data is old tho. Polygon area works

--- app.py
+++ app.py
@@ -28,6 +28,19 @@ for ln, lg, el in zip(lat, lon, elevation):
     feature_group.add_child(folium.CircleMarker(location = [ln,lg], popup =str(el) + " m", 
     fill_opacity = 0.8, fill_color = 'red', color = 'grey',  radius = 6, weight = 1,))
 
+#read the multipoylogon coordinates
+# These will map out an area/region to map out the countries by population
+#could not find one specifically for the UK at the moment
+
+feature_group.add_child(folium.GeoJson(data=open('World.json' , 'r', encoding = 'utf-8-sig').read(), 
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 100000 
+else 'orange' if 1000000 <= x['properties']['POP2005'] < 20000000 else 'red' })) #encode the data so that its readable
+
+#if the population of the polygon area is less than 100000 then it will be green
+#if the population is between 1mil and 20mil then orange else red
+
+#encode the data so that its readable
+
 map.add_child(feature_group)
 
 map.save("Map.html")
