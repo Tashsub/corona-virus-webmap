0b40c3aeb1093833c39a2d360e7e2f37d821114a
Tashinga Musanhu <44523844+Tashsub@users.noreply.github.com>
2020-08-05 23:57:57 +0200

    project complete

--- app.py
+++ app.py
@@ -3,9 +3,10 @@
 
 import folium
 import pandas
+import json
 from geopy.geocoders import Nominatim
-from folium.plugins import MarkerCluster
-from folium.features import CustomIcon
+from folium.plugins import MarkerCluster,FloatImage
+
 import os
 
 #data resource  = https://www.theguardian.com/world/2020/aug/04/coronavirus-uk-map-the-latest-deaths-and-confirmed-covid-19-cases
@@ -30,11 +31,11 @@ def calculateLatLong(city_names):
 #return the colour of teh icon and the icon
 def health_risk(caseCount):
     if caseCount > 4000:
-        return "red","remove"
+        return "red","remove","Avoid if Possible"
     elif caseCount > 2000:
-        return "orange","eye-open" 
+        return "orange","eye-open", "Watch this area closely"
     else:
-        return "green","check"
+        return "green","check","Proceed with caution"
       
 
 def convert_str_int(val):
@@ -61,13 +62,28 @@ for coord, arr, cas, pop in zip(coordinates, area_name, case_count, population):
     lat_value = (coord.split())[0]
     long_value =  (coord.split())[1]
 
-
+    
     #icon is decided on the case count of the area
-    icon_pic = 2
-
     marker_color = health_risk(cas)[0]
-    feature_group1.add_child(folium.Marker(location = [lat_value,long_value], popup = "lol", 
-    icon = folium.Icon(color=marker_color, icon_color='white', icon=health_risk(cas)[1])))
+
+    val = 4
+
+    mystring = 'Area: {}, \n Cases: {}, \n N. of people/100K: {}'.format(arr, cas, pop  )
+    print(mystring)
+
+
+    html = """
+    <h1> Area: """ + arr +   """</h1><br>
+    <p>Cases: """ + str(cas) + """</p>
+    <p>Confirmed cases/100k: """ + str(pop) + """</p>
+    <p>Conclusion: """ + health_risk(cas)[2] + """</p>
+
+
+    """
+
+    
+    feature_group1.add_child(folium.Marker(location = [lat_value,long_value], popup = html,
+    tooltip = arr ,icon = folium.Icon(color=marker_color, icon_color='white', icon=health_risk(cas)[1])))
    
 
 
@@ -84,10 +100,15 @@ else 'orange' if 1000000 <= x['properties']['POP2005'] < 20000000 else 'red' }))
 #if the population of the polygon area is less than 100000 then it will be green
 #if the population is between 1mil and 20mil then orange else red
 
-#encode the data so that its readable
+
+#add the legend 
+image_file = 'legend1.PNG'
+
+feature_group3 = fFloatImage(image_file, bottom=-5, left=85)
 
 map.add_child(feature_group1)
 map.add_child(feature_group2)
+map.add_child(feature_group3)
 
 #turn on and off feature groups
 map.add_child(folium.LayerControl())
